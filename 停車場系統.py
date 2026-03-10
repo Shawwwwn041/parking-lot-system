bfeb1792b742e2aee1da91f3b53bd1190b5ff2c3
Shawwwwn041 <yamps.1550@gmail.com>
2026-03-10 17:01:32 +0000

    測試ipad寫程式功能

--- 停車場系統.py
+++ 停車場系統.py
@@ -1,7 +1,7 @@
 #停車場系統
 import datetime #確認停車時間
-car_num = input("車牌號").upper()
-num_part = car_num.split("-")
+car_num = input("車牌號").upper()#輸入車牌號碼
+num_part = car_num.split("-") 
 if len(num_part) == 2:
     print(f"切開的結果為{num_part}")
 else:
